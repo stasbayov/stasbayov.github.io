f519840c8cd68f7413ab441030cb841540299304
Stanislav.Baiov <Stanislav.Baiov@ewave.co.il>
2020-05-13 17:24:45 +0300

    squro test

--- squro/dist/js/main.js
+++ squro/dist/js/main.js
@@ -8,9 +8,15 @@
 
 $(document).ready((function() {
     /// click languages
+    $("#lang-menu").on('click', (function () {
+        $('#lang-menu').toggleClass('opened');
+    }))
     $("#lang-menu ul li").on("click", (function(){
         //select lang and apply changes
         $lang = $(this).html();
         $("#lang-menu div").html($lang);
     }));
+    $(".nav-item").on('click', (function () {
+        $(this).toggleClass('open-submenu');
+    }))
 }));
